b62136b2afdc047c80304b4f2845d1ca3bb528a6
Vivacia <varnikabudati@gmail.com>
2021-12-01 00:19:29 -0500

    day 1 p2

--- day1.py
+++ day1.py
@@ -1,12 +1,35 @@
-file1 = open('input.txt', 'r')
-Lines = file1.readlines()
+def p1():
+    file1 = open('input.txt', 'r')
+    Lines = file1.readlines()
 
-start = -1
-curr = 0
-for line in Lines:
-    num = int(line.strip())
-    if num > curr:
-        start += 1
-    curr = num
+    start = -1
+    curr = 0
+    for line in Lines:
+        num = int(line.strip())
+        if num > curr:
+            start += 1
+        curr = num
 
-print(start)
+    print(start)
+
+def p2():
+    file1 = open('input.txt', 'r')
+    Lines = file1.readlines()
+
+    start = -1
+    curr = 0
+    for i in range(len(Lines)-2):
+        num1 = int(Lines[i])
+        num2 = int(Lines[i+1])
+        num3 = int(Lines[i+2])
+
+        sum = num1 + num2 + num3
+
+        if sum > curr:
+            start += 1
+
+        curr = sum
+
+    print(start)
+
+p2()
